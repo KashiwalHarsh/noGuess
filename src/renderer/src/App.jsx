44942c3f9bf7ef6821bba6cee2d00061fde63a2d
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2024-02-16 21:53:25 +0530

    did basic boilerplating :

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,28 +1,20 @@
 import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
 
 function App() {
   const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 
   return (
     <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
+      <h1>Steganography Data Hiding</h1>
       <div className="actions">
         <div className="action">
           <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
+            Upload Image to Encrypt
           </a>
         </div>
         <div className="action">
           <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
+            Decrypt
           </a>
         </div>
       </div>
